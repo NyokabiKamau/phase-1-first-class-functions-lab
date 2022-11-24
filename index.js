a7c215f4fa2c5ab0390128d650dc289095c3ebed
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-24 23:07:24 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+ const returnFirstTwoDrivers = (arrayFirst) => {
+    return arrayFirst.slice(0, 2)
+ }
+ const returnLastTwoDrivers = (arrayLast) => {
+    return arrayLast.slice(-2)
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ const createFareMultiplier = (n1) => {
+    return function () { return n1 * n1}
+ }
+
+ const fareDoubler = (n2) => {
+    return n2 * 2
+ }
+
+ const fareTripler = (n3) => {
+    return n3 * 3
+ }
+
+ const selectDifferentDrivers = (arrayOfDrivers, func) => {
+    return func(arrayOfDrivers)
+ }
